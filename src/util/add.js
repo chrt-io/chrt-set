8010d92b8a6f93c15ec5fd2b5b0acc80ce4116c7
carlo <littleark@gmail.com>
2021-11-01 10:37:21 +0530

    removed a leftoveer tilde (~) from the code

--- src/util/add.js
+++ src/util/add.js
@@ -1,4 +1,4 @@
-import { uuid } from '~/helpers';
+import { uuid } from '../helpers';
 export default function add(obj) {
   const id = obj._id || uuid();
   // console.log('adding to', this, obj.type, id, obj);
